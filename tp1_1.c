2100cfe77a8f18cbcfdbaae167cb7dc8d8757fb7
marisolelbafigueroa@gmail.com <marisolelbafigueroa@gmail.com>
2025-03-30 17:36:56 -0300

    segundo commit

--- tp1_1.c
+++ tp1_1.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hola mundo");
+
+    //printf("Hola mundo");
+
+    int numero=2;
+    int *puntero=&numero;
+
+    printf("Contenido del puntero:%d\n" , *puntero);
+    printf("Direccion de memoria almacenada por el puntero:%d\n" ,puntero);
+    printf("Direccion de memoria de la variable:%d\n" , &numero);
+    printf("Direccion de memoria del puntero:%d\n" , &puntero);
+    printf("Size of numero es: %lu\n", sizeof(numero));
+
 
     return 0;
 }
